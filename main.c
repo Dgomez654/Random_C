4fda8b0d0cf3abda9a167474a31de58e3fa6273a
Dgomez654 <dgomez4@milaifontanals.org>
2025-04-03 19:56:38 +0200

    Update main.c

--- main.c
+++ main.c
@@ -3,14 +3,8 @@
 #include <time.h>
 
 int main() {
-    // Inicialitza el generador de nombres aleatoris amb la hora actual
     srand(time(NULL));
-
-    // Genera un nombre aleatori entre 0 i RAND_MAX
-    int nombre_aleatori = rand();
-
-    // Imprimeix el nombre aleatori
-    printf("Nombre aleatori: %d\n", nombre_aleatori);
-
+    int numero_aleatorio = rand();
+    printf("Numero aleatorio: %d\n", numero_aleatorio);
     return 0;
 }
